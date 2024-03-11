733a01cd5e7e6f7003ac1f667858e3bb97219d21
Luciano Borba <luhborbafilho@gmail.com>
2024-03-11 15:40:49 -0300

    feat: criando rota de produtos

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 from fastapi import FastAPI
+from typing import List, Dict
 
 app = FastAPI()
+
+
+produtos : List[Dict[str, any]] = [
+    {
+        "id": 1,
+        "nome": "Smartphone",
+        "descricao": "Um telefone com funcionamento de computador",
+        "preco": 1500.00,
+        "disponivel": True,
+    },
+    {
+        "id": 2,
+        "nome": "Notebook",
+        "descricao": "Um computador com tela de 15 polegadas",
+        "preco": 3000.00,
+        "disponivel": False,
+    },
+    {
+        "id": 3,
+        "nome": "Tablet",
+        "descricao": "Um tablet com tela de 10 polegadas",
+        "preco": 2000.00,
+        "disponivel": True,
+    },
+]
 
 @app.get("/")
 def ola_mundo():
     return {"Hello": "World"}
+
+@app.get("/produtos")
+def buscar_produtos():
+    return produtos
