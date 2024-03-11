70010b50fb4ea55c96350794d9c5e55d65832e08
Luciano Borba <luhborbafilho@gmail.com>
2024-03-11 15:43:35 -0300

    feat(test): criando test para rota de produtos

--- testes.py
+++ testes.py
@@ -12,3 +12,12 @@ def test_ola_mundo():
 def test_ola_mundo_json():
     response = client.get("/")
     assert response.json() == {"Hello": "World"}
+
+
+def test_buscar_produtos_status_code():
+    reponse = client.get("/produtos")
+    assert reponse.status_code == 200
+
+def test_tamanho_buscar_produtos():
+    reponse = client.get("/produtos")
+    assert len(reponse.json()) == 3
